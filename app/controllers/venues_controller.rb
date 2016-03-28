c1ca5833e1087643abda91438161bd66b9fb95ce
Harry Nguyen <dnncntt@gmail.com>
2016-03-28 10:50:19 +0700

    finish the last required story: create venue

---
+++ app/controllers/venues_controller.rb
@@ -0,0 +1,23 @@
+class VenuesController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+  	@venue = Venue.new
+  end
+
+  def create
+  	@venue = Venue.new venue_params
+  	if @venue.save
+  		flash[:success] = "New venue has been created"
+  		redirect_to new_event_path
+  	else
+		flash[:error] = "Error: #{@venue.errors.full_messages.to_sentence}"
+  	end
+  end
+
+  private
+
+  def venue_params
+  	params.require(:venue).permit(:name,:full_address,:region_id)
+  end
+end
